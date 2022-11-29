5c6c8a1d108a527cfa0e955c3af85ade779721ee
grbm <olejnec@gmail.com>
2022-11-29 10:28:24 +0300

    2811

--- src/main/webapp/src/api/PostService.js
+++ src/main/webapp/src/api/PostService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default class PostService {
-    static request = axios.create({baseURL: "http://localhost:8081"})
+    static request = axios.create({baseURL: "http://192.168.0.100:8081"})
 
     static deletePost = async (id) => {
         await PostService.request.post(`/delete/${id}`)
